32d09610fb6a2cb975db26296727d9fde1e2c697
THYWIZARD <abdulhaqabdulganiy@gmail.com>
2025-12-10 19:32:40 +0100

    Update service worker registration path

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // register SW
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('./service-worker.js')
     .catch(err => console.warn('SW failed', err));
 }
 

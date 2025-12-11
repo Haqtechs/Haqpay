60254c42230c6336a7099f639bfdddbc9f234ea6
THYWIZARD <abdulhaqabdulganiy@gmail.com>
2025-12-11 08:15:14 +0100

    Implement service worker install event
    Added install event listener to cache assets.

--- service-worker.js
+++ service-worker.js
@@ -34,3 +34,9 @@ self.addEventListener('fetch', evt => {
     caches.match(evt.request).then(res => res || fetch(evt.request).catch(()=>caches.match('./index.html')))
   );
 });
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  console.log('Service Worker Installed');
+});
